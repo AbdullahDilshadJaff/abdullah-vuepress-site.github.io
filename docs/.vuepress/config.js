ba990844b48f742f54218cacfdd61816c921bbbc
Abdullah Dilshad <abdulla.00433619@gmail.com>
2025-10-06 02:08:18 +0300

    Fix base path for GitHub Pages

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,7 @@ import { defaultTheme } from '@vuepress/theme-default'
 import { viteBundler } from '@vuepress/bundler-vite'
 
 module.exports = ({
-   base: '/',
+base: '/abdullah-vuepress-site.github.io/',
   title: "Abdullah's VuePress Site",
   description: "A static site built with VuePress v2",
   head: [
